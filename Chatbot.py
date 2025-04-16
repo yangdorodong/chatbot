8521e0bc815e6f7232960a4c7ceff07e0f210fcf
YangDongMin <167498440+yangdorodong@users.noreply.github.com>
2025-04-16 22:53:13 +0900

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -13,12 +13,15 @@ with st.sidebar:
 
 st.title("MBTaIk")
 st.caption("원하는 MBTI 스타일을 말해봐!")
+system_prompt = f"""너는 원하는 MBTI에 맞게 고민상담을 해주는 챗봇이야.
+이 데이터는 16가지 mbti의 특징을 보여줘{few-shot_data}
+"""
 if "messages" not in st.session_state:
     
     st.session_state["messages"] = [
         {
             "role": "system", 
-            "content": "당신은 MBTI별 고민을 들어주는 챗봇입니다."
+            "content": system_prompt
         },
         {"role": "assistant", "content": "원하는 MBTI가 있어요?"}
                                    ]
